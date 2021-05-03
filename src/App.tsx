9452a4d98849c1e385edabc7e96ae8655b678b41
Zak-11 <dianazakov@rambler.ru>
2021-05-04 00:30:46 +0300

    localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Counter} from "./Counter/Counter";
 
 
@@ -7,6 +7,25 @@ export function App() {
     let maxValue = 5
 
     const [count, setCount] = useState(0)
+
+   useEffect(() => {
+
+       let valueAsString = localStorage.getItem('counterValue')
+       if (valueAsString){
+           let newValue = JSON.parse(valueAsString)
+           setCount(newValue)
+       }
+    },[])
+
+
+    useEffect(() =>{
+        localStorage.setItem('counterValue', JSON.stringify(count))
+
+
+    })
+
+
+
 
     let increment = () => {
 
